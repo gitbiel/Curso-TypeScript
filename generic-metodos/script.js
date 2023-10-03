9e47da613088ce1a8b631a2842025d4ce60c88a8
Gabriel Mendes <gabrielzmendes1@gmail.com>
2023-10-02 21:07:13 -0300

    aula generic metodos

---
+++ generic-metodos/script.js
@@ -0,0 +1,12 @@
+"use strict";
+// Define que o retorno será um HTMLAnchorElement
+const LInk = document.querySelector('.link');
+LInk?.href;
+async function getData(url) {
+    const response = await fetch(url);
+    return await response.json();
+}
+async function handleData() {
+    const notebook = await getData('https://api.origamid.dev/json/notebook.json');
+    console.log(notebook.nome);
+}
---
+++ generic-metodos/script.ts
@@ -0,0 +1,19 @@
+// Define que o retorno será um HTMLAnchorElement
+const LInk = document.querySelector<HTMLAnchorElement>('.link');
+LInk?.href;
+
+async function getData<T>(url: string): Promise<T> {
+  const response = await fetch(url);
+  return await response.json();
+}
+
+interface Notebook {
+  nome: string;
+}
+
+async function handleData() {
+  const notebook = await getData<Notebook>(
+    'https://api.origamid.dev/json/notebook.json',
+  );
+  console.log(notebook.nome);
+}
