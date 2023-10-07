ac8ddf1d79e97ba2f6a2079861ea11ba93f9ca5f
Gabriel Mendes <gabrielzmendes1@gmail.com>
2023-10-06 22:47:55 -0300

    function void

--- function-01/script.js
+++ function-01/script.js
@@ -1,8 +1,16 @@
 "use strict";
-function somar(a, b, c) {
-  return a + b + (c ? c : 0);
+function Somar(a, b, c) {
+    return a + b + (c ? c : 0);
 }
-somar(3, 4);
-somar(3, 4, 1);
+console.log(Somar(3, 4));
+console.log(Somar(3, 4, 1));
 const subtrair = (a, b) => a - b;
-subtrair(10, 2);
+console.log(subtrair(10, 2));
+/*
+  No JavaScript, uma função sem return irá retornar undefined. No TypeScript o retorno é definido
+  como void. Isso evita usos errados como checagens booleans de métodos que não possuem retorno
+*/
+function pintarTela(cor) {
+    document.body.style.background = cor;
+}
+console.log(pintarTela("gray"));
--- function-01/script.ts
+++ function-01/script.ts
@@ -1,11 +1,23 @@
-function somar(a: number, b: number, c?: number): number {
+function Somar(a: number, b: number, c?: number): number {
   return a + b + (c ? c : 0);
 }
-somar(3, 4);
-somar(3, 4, 1);
+
+console.log(Somar(3, 4));
+console.log(Somar(3, 4, 1));
 
 const subtrair = (a: number, b: number): number => a - b;
 
-subtrair(10, 2);
+console.log(subtrair(10, 2));
 
 type Callback = (event: MouseEvent) => void;
+
+/*
+  No JavaScript, uma função sem return irá retornar undefined. No TypeScript o retorno é definido 
+  como void. Isso evita usos errados como checagens booleans de métodos que não possuem retorno  
+*/
+
+function pintarTela(cor: string) {
+  document.body.style.background = cor;
+}
+
+console.log(pintarTela("gray"))
