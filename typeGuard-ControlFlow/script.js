a68476162bb385d61c1c7773aaca7ae11830bd7d
Gabriel Mendes <gabrielzmendes1@gmail.com>
2023-10-10 23:52:12 -0300

    Guard, Safety e Narrowing

---
+++ typeGuard-ControlFlow/script.js
@@ -0,0 +1,24 @@
+"use strict";
+/*
+  Guard, Safety e Narrowing
+
+  O Type Guard (defesa) garante a Type Safety (segurança) do dado dentro do bloco condicional.
+  Esse processo é chamado de Type Narrowing (estreitamento).
+
+  O TypeScript faz Control Flow (controle de fluxo) para entender qual o dado dentro
+  da condicional.
+*/
+function typeGuard(value) {
+    if (typeof value === "string") {
+        return value.toLowerCase();
+    }
+    if (typeof value === "number") {
+        return value.toFixed();
+    }
+    if (value instanceof HTMLElement) {
+        return value.innerText;
+    }
+}
+typeGuard("ORIGAMID");
+typeGuard(10);
+typeGuard(document.body);
