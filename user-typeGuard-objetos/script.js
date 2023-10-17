f794577d5bfe8aff4a05638eb5e99b36649e4d04
Gabriel Mendes <gabrielzmendes1@gmail.com>
2023-10-16 23:56:36 -0300

    user type guard 02

---
+++ user-typeGuard-objetos/script.js
@@ -0,0 +1,30 @@
+"use strict";
+/*
+  Objetos
+
+  O Type Predicate pode ser especialmente utilizado para criarmos Type Guards
+  para objetos específicos e garantirmos a Type Safety (segurança) do projeto.
+*/
+async function fetchProduto() {
+  const response = await fetch("https://api.origamid.dev/json/notebook.json");
+  const json = await response.json();
+  handleProduto(json);
+}
+fetchProduto();
+function isProduto(value) {
+  if (
+    value &&
+    typeof value === "object" &&
+    "nome" in value &&
+    "preco" in value
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function handleProduto(data) {
+  if (isProduto(data)) {
+    console.log(data);
+  }
+}
---
+++ user-typeGuard-objetos/script.ts
@@ -0,0 +1,39 @@
+/* 
+  Objetos
+
+  O Type Predicate pode ser especialmente utilizado para criarmos Type Guards
+  para objetos específicos e garantirmos a Type Safety (segurança) do projeto.
+*/
+
+async function fetchProduto() {
+  const response = await fetch("https://api.origamid.dev/json/notebook.json");
+  const json = await response.json();
+  handleProduto(json);
+}
+fetchProduto();
+
+interface Priduto {
+  nome: string;
+  preco: number;
+}
+
+function isProduto(value: unknown): value is Produto {
+  if (
+    value &&
+    typeof value === "object" &&
+    "nome" in value &&
+    "preco" in value
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function handleProduto(data: unknown) {
+  if (isProduto(data)) {
+    if (typeof data.nome === "string") {
+      console.log(data.nome.toUpperCase());
+    }
+  }
+}
