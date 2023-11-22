a56928a9a1b93587bb45e4b237242e5781a04e66
Gabriel Mendes <gabrielzmendes1@gmail.com>
2023-11-22 17:48:24 -0300

    module

---
+++ module-ts/script.js
@@ -0,0 +1,16 @@
+/*
+  module
+
+  Se usarmos o import/export em qualquer momento em um arquivo. ts,
+    TypeScript irá tratar o mesmo como o module.
+  Consequentemente, o seu escopo não será mais global.
+*/
+// pluginSlide.ts
+function pluginSlide(seletor) {
+    console.log(`Criar slide: ${seletor}`);
+}
+export default pluginSlide;
+// global.ts
+const URL_BASE = 'https://api.origamid.dev/json';
+pluginSlide('div');
+console.log(URL_BASE);
